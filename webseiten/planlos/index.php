7dfeca7e2175696e437d375f69dbce0b8551a456
Cyber_sKO <cyber.sko@gmail.com>
2016-04-26 22:40:56 +0200

    Webseite Planlos | hinzugefügt, läuft

---
+++ webseiten/planlos/index.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.jpg" type="image/jpg >
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+<meta charset="UTF-8">
+<meta name="author" content="Cyber_sKO">
+<title>Planlos - Coc Clan</title>
+<style type="text/css">
+ <!--
+	ul#navigation {
+		margin: 0px;
+		padding: 0px;
+		list-style-type: none;
+    }
+
+	ul#navigation li {
+		padding: 0px;
+		margin: 0px;
+		display: inline;
+	}
+
+	ul#navigation li a:link {
+		border: 1px solid #000000;
+		background-color: #FF0040;
+		padding: 12px 15px 12px 15px;
+		font-size: 18px;
+		color: #000;
+		font-family: Arial, Helvetica, sans-serif;
+		text-decoration: none;
+		font-weight: bold;
+	}
+
+	ul#navigation li a:hover {
+		border: 1px solid #000;
+		background-color: #0861FB;
+		padding: 12px 15px 12px 15px;
+		color: #fff;
+	}
+ -->
+</style>
+</head>
+<body style="background-image:url(black_lines.jpg)" alink="black" vlink="black">
+<div align="center">
+</br>
+<h1><font color="red"><u>Planlos - Clash of Clans Clan</u></font></h1>
+<hr>
+</br>
+<ul id="navigation">
+		<li><a href="http://localhost/index.php">Startseite</a></li>
+		<li><a href="http://localhost/kontakt.php">Kontakt</a></li>
+</ul>
+</br>
+<hr>
+<br>
+<iframe src="https://clashofclans.com/clans/clan?clanTag=LV20J9CR" width="90%" height="600" name="ClanInfoCoC" frameborder="0" scrolling="yes">
+	<p>Ihr Browser kann leider keine eingebetteten Frames anzeigen</p>
+</iframe>
+<br>
+</br>
+<hr>
+</br><font size="10" color="red">Infos &Uuml;ber den Clan</font>
+<br>
+<ul id="infoclan"><font color="green" size="5">
+	<li><b><u>Name:</u></b> Planlos</li>
+	<li><b><u>Tag:</u></b> #LV20J9CR</li>
+	<li><b><u>Level:</u></b> 3</li>
+	<li><b><u>Punkte:</u></b> 1339/1900</li>
+	<li><b><u>GesamtPunkte:</u></b> ~18'500</li>
+	<li><b><u>Gewonnene Kriege:</u></b> 8</li>
+	<li><b><u>Siegesserie:</u></b> 1</li>
+	<li><b><u>Mitglieder:</u></b> 49/50</li>
+	<li><b><u>Art:</u></b> Nur auf Einladung</li>
+	<li><b><u>Ben&ouml;tigte Troph&auml;en:</u></b> 1200</li>
+	<li><b><u>Kriegsh&auml;ufigkeit:</u></b> Zweimal pro Woche</li>
+	<li><b><u>Clanregion:</u></b> Germany/Deutsch</li>
+</font></ul>
+<br>
+<hr>
+<br>
+<font color="red" size="12">Regeln</font>
+<font color="green" size="5">
+<ol id="regeln">
+	<li>respektvoller und freundlicher Umgang</li>
+	<li>aktiv sein</li>
+</ol></font>
+</br>
+<hr>
+<br>
+<font color="red" size="10">Anf&uuml;hrer und VizeAnf&uuml;hrer</font></br>
+<font color="green" size="5">
+<ul id="verwalter">
+	<li><b><font color="red">Anf&uuml;hrer:</font> </b> Alexlovenina</li>
+	<font color="red"><b>Vizeanf&uuml;hrer</b></font>
+	<li>ZzeusS</li>
+	<li>Ryg</li>
+	<li>florian628</li>
+	<li>STEVOK27</li>
+	<li><< RAINBOW-SIX >></li>
+	<li>justin15</li>
+	<li>VFB M&uuml;mmel II</li>
+	<li>tati</li>
+	<li>Mr.Micha</li>
+</ul>
+</font>
+</br>
+<hr>
+<br>
+<font size="15" color="red">Aufstellungen</font>
+<br><font color="green" size="5" >
+<br><u>Aufstellung von Alexlovenina</u></br>
+<img src="./aufstellungen/alex.png" /><br></br>
+<br><u>Aufstellung von ZzeusS</u></br>
+<img src="./aufstellungen/zeus.png" /><br></br>
+<br><u>Aufstellung von Ryg</u></br>
+<img src="./aufstellungen/ryg.png" /><br></br>
+<br><u>Aufstellung von florian628</u></br>
+<img src="./aufstellungen/florian.png" /><br></br>
+<br><u>Aufstellung von STEVOK27</u></br>
+<img src="./aufstellungen/stevok.png" /><br></br>
+<br><u>Aufstellung von << RAINBOW-SIX >></u></br>
+<img src="./aufstellungen/rainbow.png" /><br></br>
+<br><u>Aufstellung von VFB M&uuml;mmel II</u></br>
+<img src="./aufstellungen/vfb.png" /><br></br>
+<br><u>Aufstellung von tati</u></br>
+<img src="./aufstellungen/tati.png" /><br></br>
+<br><u>Aufstellung von Mr.Micha</u></br>
+<img src="./aufstellungen/micha.png" /><br></br>
+<br>
+<hr>
+</font></div>
+</body>
+</html>
---
+++ webseiten/planlos/kontakt.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.jpg" type="image/jpg >
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+<meta charset="UTF-8">
+<meta name="author" content="Cyber_sKO">
+<title>Planlos - Coc Clan</title>
+<style type="text/css">
+ <!--
+	ul#navigation {
+		margin: 0px;
+		padding: 0px;
+		list-style-type: none;
+    }
+
+	ul#navigation li {
+		padding: 0px;
+		margin: 0px;
+		display: inline;
+	}
+
+	ul#navigation li a:link {
+		border: 1px solid #000000;
+		background-color: #FF0040;
+		padding: 12px 15px 12px 15px;
+		font-size: 18px;
+		color: #000;
+		font-family: Arial, Helvetica, sans-serif;
+		text-decoration: none;
+		font-weight: bold;
+	}
+
+	ul#navigation li a:hover {
+		border: 1px solid #000;
+		background-color: #0861FB;
+		padding: 12px 15px 12px 15px;
+		color: #fff;
+	}
+ -->
+</style>
+</head>
+<body style="background-image:url(black_lines.jpg)" alink="black" vlink="black">
+<div align="center">
+</br>
+<hr>
+<h1><font color="red"><u>Planlos - Clash of Clans Clan</u></font></h1>
+</br>
+<ul id="navigation">
+		<li><a href="http://localhost/index.php">Startseite</a></li>
+		<li><a href="http://localhost/kontakt.php">Kontakt</a></li>
+</ul>
+</br>
+<hr>
+<font color="red"><h1>Kontaktinformationen</h1></font>
+<hr>
+<font size="14" color="red"><b>Anf&uuml;hrer:</b></font><br>
+<font size="5" color="green">
+<b><u>Ingame Name:</u></b> Alexlovenina</br>
+<b><u>Name:</u></b> Alex</br>
+<b><u>EMail:</u></b> Alexkhol@yahoo.com</br></font>
+<br>
+<hr>
+<br>
+<font size="14" color="red"><b>WebseitenAdmin:</b></font><br>
+<font size="5" color="green">
+<b><u>Ingame Name:</u></b> Stevok27 & Onkel Dolf</br>
+<b><u>Name:</u></b> Florian</br>
+<b><u>Alias:</u></b> Cyber_sKO</br>
+<b><u>EMail:</u></b> cyber.sko@gmail.com</br></font>
+<br>
+<hr>
+</div>
+</body>
